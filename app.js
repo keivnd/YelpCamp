697624ecca646d17a5dc8db2893f212d0e48cd83
Kevin Deerajviset <kevin.deerajviset@gmail.com>
2017-09-15 14:00:24 +1000

    upaded app.js to render the files

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
+  var campgrounds = [
+    {name: "Salmon Creek", image: "https://image.shutterstock.com/z/stock-photo-tourists-in-the-camp-at-rest-enjoy-nature-and-food-211136374.jpg"},
+    {name: "Tuna Creek", image: "https://image.shutterstock.com/z/stock-photo-tourists-in-the-camp-at-rest-enjoy-nature-and-food-211136374.jpg"},
+    {name: "Shark Creek", image: "https://image.shutterstock.com/z/stock-photo-tourists-in-the-camp-at-rest-enjoy-nature-and-food-211136374.jpg"}
+  ]
+
+  res.render("campgrounds", {campgrounds:campgrounds});
+});
+
 app.listen(3000, process.env.IP, function() {
   console.log("YelpCamp Server Has Started!");  
 });
