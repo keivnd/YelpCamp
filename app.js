b866b5f7301362a9049e885f076b4c60f167da34
Kevin Deerajviset <kevin.deerajviset@gmail.com>
2017-09-18 12:40:50 +1000

    added data persistency

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+// SCHEMA SETUP
+var campgroundsSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundsSchema);
 
 var campgrounds = [
   {name: "Salmon Creek", image: "https://image.shutterstock.com/z/stock-photo-tourists-in-the-camp-at-rest-enjoy-nature-and-food-211136374.jpg"},
@@ -16,8 +26,14 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res) {
-
-  res.render("campgrounds", {campgrounds:campgrounds});
+  // get all campgrounds from db
+  Campground.find({}, function(err, allCampgrounds) {
+      if(err) {
+          console.log(err);
+      } else {
+          res.render("campgrounds", {campgrounds:allCampgrounds});
+      }
+  });
 });
 
 app.post("/campgrounds", function(req, res) {
@@ -26,9 +42,15 @@ app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = {name: name, image: image};
-  campgrounds.push(newCampground);
-  // redirect back to campgrounds page
-  res.redirect("/campgrounds");
+  // save campground to db
+  Campground.create(newCampground, function(err, newlyCreated) {
+      if(err) {
+          console.log(err);
+      } else {
+          // redirect back to campgrounds page
+          res.redirect("/campgrounds");
+      }
+  });
 });
 
 app.get("/campgrounds/new", function(req, res) {
